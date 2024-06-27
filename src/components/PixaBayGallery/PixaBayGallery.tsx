2bbf2eb20a5cf416ff8e4ea62cc65e6a6db0d0ef
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-06-28 01:24:59 +0300

    1 Create cli component PixaBayGallery

---
+++ src/components/PixaBayGallery/PixaBayGallery.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { PixaBayGalleryWrapper } from './PixaBayGallery.styled';
+
+interface PixaBayGalleryProps {}
+
+const PixaBayGallery: FC<PixaBayGalleryProps> = () => (
+ <PixaBayGalleryWrapper>
+    PixaBayGallery Component
+ </PixaBayGalleryWrapper>
+);
+
+export default PixaBayGallery;
