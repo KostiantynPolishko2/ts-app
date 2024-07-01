c8260c8e18d0889731e6ca459428ba08a23ebcbc
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-01 12:01:20 +0300

    1 Was created component Menu based --template typescript

---
+++ src/components/Menu/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FC, ReactElement } from 'react';
+import { Link } from 'react-router-dom'
+
+const Home: FC = (): ReactElement => {
+    return(
+        <div>
+            Home page
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { MenuWrapper } from './Menu.styled';
+
+interface MenuProps {}
+
+const Menu: FC<MenuProps> = () => (
+   <MenuWrapper>
+      Menu Component
+   </MenuWrapper>
+);
+
+export default Menu;
