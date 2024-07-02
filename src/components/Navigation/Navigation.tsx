25aede155c618a3b947218e6862d382689957a4b
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-02 11:01:08 +0300

    1 FC Navigation was been created

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { NavigationWrapper } from './Navigation.styled';
+
+interface NavigationProps {}
+
+const Navigation: FC<NavigationProps> = () => (
+ <NavigationWrapper>
+    Navigation Component
+ </NavigationWrapper>
+);
+
+export default Navigation;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+import Navigation from './components/Navigation/Navigation';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Navigation />
   </React.StrictMode>
 );
 
