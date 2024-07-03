eb17654839fffff72e1ba4bd0eec31666da1a5ba
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-03 19:15:55 +0300

    3 Has been tested intaraction css styles of node with custom.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,33 +1,40 @@
 import { FC } from 'react';
 import { Display, NavigationWrapper } from './Navigation.styled';
 import 'the-new-css-reset';
+import './Navigation.css';
 
 import Pagimagic from 'react-pagimagic'
 
 interface NavigationProps {}
+interface ArrowProps {}
 
 const Navigation: FC<NavigationProps> = () => {
    const users = ['home', 'contact', 'about', 'hello', 'world', '2024', 'console', 'log', 'C++', 'WinAdmin', 'C#', 'HTML', 'CSS', 'JS', 'TS', 'React'];
 
-   const myArr = (array: [])  => {
+   const myArr = (array: Array<string>)  => {
       return array.map(item => {       
          return (
-         <NavigationWrapper>
+         <NavigationWrapper className='card-currency'>
             <p>card {item}</p>
          </NavigationWrapper>);
       });
    }
+
+   const Forward: FC<ArrowProps> = () => <span className='my-arrow'><p>GO TO</p></span>
 
    return (
       <>
          <h3>Pagimagic module test</h3>
             <Pagimagic
                list={users}
-               itemsPerPage={3}
+               itemsPerPage={5}
                currentPageIndex={0}
-               className="your-class-if-its-necessary"
+               className="full-view"
                maximumVisiblePaginators={3}
+               showCounter={true}
+               arrow={Forward}
                renderChildren={myArr}
+               useDefaultStyles={true}
             >
             </Pagimagic>
       </>
