82a23c2333caad625c604bbded9bffbc04a7b10d
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-07-03 09:10:19 +0300

    2 Has been done initial settings for tsconfig.json and definated declaration.

--- src/components/Navigation/Navigation.styled.ts
+++ src/components/Navigation/Navigation.styled.ts
@@ -1,4 +1,19 @@
 import styled from 'styled-components';
 
+export const Display = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`;
+
 export const NavigationWrapper = styled.div`
+    width: fit-content;
+    margin: 5px;
+    padding: 2px 5px;
+    border: 1px solid black;
+    background-color: yellow;
+    & > p {
+        color: blue;
+    }
 `;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,37 @@
-import React, { FC } from 'react';
-import { NavigationWrapper } from './Navigation.styled';
+import { FC } from 'react';
+import { Display, NavigationWrapper } from './Navigation.styled';
+import 'the-new-css-reset';
+
+import Pagimagic from 'react-pagimagic'
 
 interface NavigationProps {}
 
-const Navigation: FC<NavigationProps> = () => (
- <NavigationWrapper>
-    Navigation Component
- </NavigationWrapper>
-);
+const Navigation: FC<NavigationProps> = () => {
+   const users = ['home', 'contact', 'about', 'hello', 'world', '2024', 'console', 'log', 'C++', 'WinAdmin', 'C#', 'HTML', 'CSS', 'JS', 'TS', 'React'];
+
+   const myArr = (array: [])  => {
+      return array.map(item => {       
+         return (
+         <NavigationWrapper>
+            <p>card {item}</p>
+         </NavigationWrapper>);
+      });
+   }
+
+   return (
+      <>
+         <h3>Pagimagic module test</h3>
+            <Pagimagic
+               list={users}
+               itemsPerPage={3}
+               currentPageIndex={0}
+               className="your-class-if-its-necessary"
+               maximumVisiblePaginators={3}
+               renderChildren={myArr}
+            >
+            </Pagimagic>
+      </>
+   );
+}
 
 export default Navigation;
